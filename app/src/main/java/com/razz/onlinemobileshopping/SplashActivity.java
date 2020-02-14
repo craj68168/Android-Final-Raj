cefff8522361b1890363442d847223bccccdd3eb
RaZz Chaudhary <55077982+craj68168@users.noreply.github.com>
2020-02-14 11:01:15 +0545

    finish

--- app/src/main/java/com/razz/onlinemobileshopping/SplashActivity.java
+++ app/src/main/java/com/razz/onlinemobileshopping/SplashActivity.java
@@ -13,6 +13,20 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-               
+                Handler handler = new Handler();
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this,
+                                LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                },3000);
+            }
+        }
+
+
 
 
