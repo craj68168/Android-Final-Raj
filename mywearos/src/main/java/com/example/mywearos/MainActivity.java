f1f1f93ea7815fe7ec31aac4d2cb07bd9174fb1a
RaZz Chaudhary <55077982+craj68168@users.noreply.github.com>
2020-02-14 11:02:51 +0545

    wear os checked

---
+++ mywearos/src/main/java/com/example/mywearos/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.mywearos;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.TextView;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+}
