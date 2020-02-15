f43d283375bcfbd9b79d0c739b4c869b186de485
RaZz Chaudhary <55077982+craj68168@users.noreply.github.com>
2020-02-15 11:03:49 +0545

    wear os updated

--- mywearos/src/main/java/com/example/mywearos/MainActivity.java
+++ mywearos/src/main/java/com/example/mywearos/MainActivity.java
@@ -9,4 +9,13 @@ public class MainActivity extends WearableActivity {
     private TextView mTextView;
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
 }
